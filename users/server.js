83073734998297bfe7d32dfb49226d1f0e55cf20
Mila Hose <mila.hose@gmail.com>
2022-01-31 18:19:19 -0800

    Require schema

--- users/server.js
+++ users/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema')
 const app = express();
 const PORT = 3000;
 
 app.use('/graphql', graphqlHTTP({
+  schema,
   graphiql: true
 }));
 
